d68cb7e02745d35bf5c9d184105c26b002f1d3ae
Siddhi Jain <jainsiddhi03@gmail.com>
2024-06-03 15:45:02 +0530

    Added design for profile page with chat box

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/homePage/HomePage'
 import ListPage from './pages/listPage/ListPage'
 import Layout from "./pages/layout/Layout"
 import SinglePage from "./pages/singlePage/SinglePage";
+import ProfilePage from "./pages/profilePage/ProfilePage";
 
 function App() {
   const router = createBrowserRouter([
@@ -22,6 +23,10 @@ function App() {
           path:"/:id",
           element: <SinglePage />
         },
+        {
+          path:"/profile",
+          element: <ProfilePage />
+        }
 
       ]
     },
